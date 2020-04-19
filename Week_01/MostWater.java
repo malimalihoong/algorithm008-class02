1d21a7c10cbb56692d5749cb6285bcb7085c5e21
zhangduo <18309217363@163.com>
2020-04-19 21:37:04 +0800

    盛水最多的容器

---
+++ Week_01/MostWater.java
@@ -0,0 +1,22 @@
+
+public class MostWater {
+	public int mostWater(int[] a) {
+		int max = 0;
+		for(int i = 0; i < a.length-1; ++i) {
+			for(int j = 1; j < a.length; ++j) {
+				int area = (j - i) * Math.min(a[i], a[j]);
+				max = Math.max(max, area);
+			}
+		}
+		return max;
+	}
+	
+	public int mostWater2(int[] a) {
+		int max = 0;
+		for(int i = 0, j = a.length-1; i < a.length; ++i) {
+			int area = (j-i+1)*Math.min(a[i++], a[j--]);
+			max = Math.max(max, area);
+		}
+		return max;
+	}
+}
